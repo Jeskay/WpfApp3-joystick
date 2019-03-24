b3d4c39cff42fab65f451495a68642e160a8dba7
Jeskay <37577958+Jeskay@users.noreply.github.com>
2019-03-24 14:49:54 +1000

    multicamera

---
+++ WpfApp3-joystick/Calculation/CalculationModel.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp3_joystick
+{
+    public class CalculationModel
+    {
+        private double volume;
+        private double r1;
+        private double r2;
+        private double r3;
+        private double density;
+        private double lenght;
+
+        public double Volume
+        {
+            get
+            {
+                return volume;
+            }
+            set
+            {
+                volume = value;
+            }
+        }
+        public double R1
+        {
+            get
+            {
+                return r1;
+            }
+            set
+            {
+                r1 = value;
+            }
+        }
+        public double R2
+        {
+            get
+            {
+                return r2;
+            }
+            set
+            {
+                r2 = value;
+            }
+        }
+        public double R3
+        {
+            get
+            {
+                return r3;
+            }
+            set
+            {
+                r3 = value;
+            }
+        }
+        public double Density
+        {
+            get
+            {
+                return density;
+            }
+            set
+            {
+                density = value;
+            }
+        }
+        public double Lenght
+        {
+            get
+            {
+                return lenght;
+            }
+            set
+            {
+                lenght = value;
+            }
+        }
+    }
+}
---
+++ WpfApp3-joystick/Main/MainView.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace WpfApp3_joystick
+{
+    public class MainView
+    {
+        MainModel mainModel;
+
+        public VideoCapture RulerCamera
+        {
+            get { return mainModel.RulerCamera; }
+            set { mainModel.RulerCamera = value; }
+        }
+        public VideoCapture RecognitionCamera
+        {
+            get { return mainModel.RecognitionCamera; }
+            set { mainModel.RecognitionCamera = value; }
+        }
+
+        public MainView(MainModel mainModel)
+        {
+            this.mainModel = mainModel;
+        }
+    }
+}
